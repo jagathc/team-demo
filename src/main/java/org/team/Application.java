1d6d297ea184e13985a6adce28d47fdc6797156d
Jagath Dissanayaka <jagathc@gmail.com>
2019-08-24 23:28:52 +1000

    C1

--- src/main/java/org/team/Application.java
+++ src/main/java/org/team/Application.java
@@ -1,13 +1,20 @@
 package org.team;
 
+import java.util.Map;
+
+import org.team.data.TeamPreferenceData;
+
 public class Application {
 
-	public void processData() {
+	public void processData(Map<String, String> choice) {
 		
 	}
 	
 	public static void main(String[] args) {
-		new Application().processData();
+		Application app = new Application();
+		app.processData(TeamPreferenceData.CHOICE1);
+		app.processData(TeamPreferenceData.CHOICE2);
+		app.processData(TeamPreferenceData.CHOICE3);
 	}
 
 }
